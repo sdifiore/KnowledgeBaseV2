0582008ab0a564d77d1f8ffc5e44f3e530f09eeb
Sergio Di Fiore <sergiodifiore@gmail.com>
2022-06-20 13:44:21 -0300

    Autores ok

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Knowledgebase.Data;
+using Knowledgebase.Models;
+
+namespace Knowledgebase.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Autores
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Autor.Include(a => a.Artigo);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Autores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Autor == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autor
+                .Include(a => a.Artigo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autores/Create
+        public IActionResult Create()
+        {
+            ViewData["ArtigoId"] = new SelectList(_context.Artigo, "Id", "Chamada");
+            return View();
+        }
+
+        // POST: Autores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ArtigoId")] Autor autor)
+        {
+            _context.Add(autor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Autores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Autor == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autor.FindAsync(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArtigoId"] = new SelectList(_context.Artigo, "Id", "Chamada", autor.ArtigoId);
+            return View(autor);
+        }
+
+        // POST: Autores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ArtigoId")] Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return NotFound();
+            }
+
+            _context.Update(autor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Autores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Autor == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autor
+                .Include(a => a.Artigo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Autor == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Autor'  is null.");
+            }
+            var autor = await _context.Autor.FindAsync(id);
+            if (autor != null)
+            {
+                _context.Autor.Remove(autor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutorExists(int id)
+        {
+          return (_context.Autor?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
